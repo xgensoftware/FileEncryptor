9f8dfbfd04ff504941a2e57f87c7e8a2cd89405e
DESKTOP-VMD815T\asanf <asanfilippo@xgensoftware.com>
2017-12-23 14:57:18 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace EncryptionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtToEncrypt = new System.Windows.Forms.TextBox();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.txtEncryptionKey = new System.Windows.Forms.TextBox();
+            this.btnEncryptFldDlg = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Folder:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Passphrase:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output Folder:";
+            // 
+            // txtToEncrypt
+            // 
+            this.txtToEncrypt.Location = new System.Drawing.Point(108, 19);
+            this.txtToEncrypt.Name = "txtToEncrypt";
+            this.txtToEncrypt.Size = new System.Drawing.Size(267, 20);
+            this.txtToEncrypt.TabIndex = 3;
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Location = new System.Drawing.Point(108, 62);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.Size = new System.Drawing.Size(267, 20);
+            this.txtOutputFolder.TabIndex = 4;
+            // 
+            // txtEncryptionKey
+            // 
+            this.txtEncryptionKey.Location = new System.Drawing.Point(108, 105);
+            this.txtEncryptionKey.Name = "txtEncryptionKey";
+            this.txtEncryptionKey.Size = new System.Drawing.Size(267, 20);
+            this.txtEncryptionKey.TabIndex = 5;
+            this.txtEncryptionKey.UseSystemPasswordChar = true;
+            // 
+            // btnEncryptFldDlg
+            // 
+            this.btnEncryptFldDlg.Location = new System.Drawing.Point(381, 19);
+            this.btnEncryptFldDlg.Name = "btnEncryptFldDlg";
+            this.btnEncryptFldDlg.Size = new System.Drawing.Size(29, 20);
+            this.btnEncryptFldDlg.TabIndex = 6;
+            this.btnEncryptFldDlg.Text = "...";
+            this.btnEncryptFldDlg.UseVisualStyleBackColor = true;
+            this.btnEncryptFldDlg.Click += new System.EventHandler(this.btnEncryptFldDlg_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(381, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 20);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(173, 151);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 8;
+            this.btnProcess.Tag = "0";
+            this.btnProcess.Text = "Encrypt";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.RunProcess_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(335, 151);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(254, 151);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 10;
+            this.btnDecrypt.Tag = "1";
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.RunProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 190);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEncryptFldDlg);
+            this.Controls.Add(this.txtEncryptionKey);
+            this.Controls.Add(this.txtOutputFolder);
+            this.Controls.Add(this.txtToEncrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtToEncrypt;
+        private System.Windows.Forms.TextBox txtOutputFolder;
+        private System.Windows.Forms.TextBox txtEncryptionKey;
+        private System.Windows.Forms.Button btnEncryptFldDlg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnDecrypt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using com.Xgensoftware.Core;
+
+namespace EncryptionTest
+{
+    public partial class Form1 : Form
+    {
+        public readonly byte[] salt = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; 
+        public const int iterations = 1042;
+        Logging _log = null;
+        List<string> _filesToencrypt = new List<string>();        
+        List<Task> _tasks;
+        string _password = string.Empty;
+        #region Private Methods 
+        private void DisableControls()
+        {
+            txtToEncrypt.Enabled = false;
+            txtOutputFolder.Enabled = false;
+            txtEncryptionKey.Enabled = false;
+            btnProcess.Enabled = false;
+            btnDecrypt.Enabled = false;
+        }
+
+        private void EnableControls()
+        {
+            txtToEncrypt.Enabled = true;
+            txtOutputFolder.Enabled = true;
+            txtEncryptionKey.Enabled = true;
+            btnProcess.Enabled = true;
+            btnDecrypt.Enabled = true;
+        }
+
+        private void WriteLog(string message)
+        {
+            string logMsg = string.Format("{0}:  {1}", DateTime.Now.ToString("yyyyMMdd HH:mm:ss"), message);
+            _log.LogMessage(Logging.LogMessageType.INFO, logMsg);
+        }
+
+        private void Encrypt(string inputFile, string outputFile)
+        {
+            try
+            {
+                AesManaged aes = new AesManaged();
+                aes.BlockSize = aes.LegalBlockSizes[0].MaxSize;
+                aes.KeySize = aes.LegalKeySizes[0].MaxSize;
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(_password, salt, iterations);
+                aes.Key = key.GetBytes(aes.KeySize / 8);
+                aes.IV = key.GetBytes(aes.BlockSize / 8);
+                aes.Mode = CipherMode.CBC;
+                ICryptoTransform transform = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (FileStream destination = new FileStream(outputFile, FileMode.CreateNew, FileAccess.Write, FileShare.None))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(destination, transform, CryptoStreamMode.Write))
+                    {
+                        using (FileStream source = new FileStream(inputFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+                        {
+                            source.CopyTo(cryptoStream);
+
+                            WriteLog(string.Format("Encrypted: {0} : {1}", inputFile, outputFile));
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                WriteLog(string.Format("Failed to encrypt {0}.ERROR: {1}",inputFile,ex.Message));
+            }
+            
+        }
+
+        private void Decrypt(string inputFile, string outputFile)
+        {
+            try
+            {
+                AesManaged aes = new AesManaged();
+                aes.BlockSize = aes.LegalBlockSizes[0].MaxSize;
+                aes.KeySize = aes.LegalKeySizes[0].MaxSize;
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(_password, salt, iterations);
+                aes.Key = key.GetBytes(aes.KeySize / 8);
+                aes.IV = key.GetBytes(aes.BlockSize / 8);
+                aes.Mode = CipherMode.CBC;
+                ICryptoTransform transform = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (FileStream destination = new FileStream(outputFile, FileMode.CreateNew, FileAccess.Write, FileShare.None))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(destination, transform, CryptoStreamMode.Write))
+                    {
+                        using (FileStream source = new FileStream(inputFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+                        {
+                            source.CopyTo(cryptoStream);
+                            WriteLog(string.Format("Decrypting: {0} : {1}", inputFile, outputFile));
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                WriteLog(string.Format("Failed to decrypt {0}.ERROR: {1}", inputFile, ex.Message));
+            }
+        }
+
+        public void CreateZipFile(string fileName, DirectoryInfo source)
+        {
+            // Create and open a new ZIP file
+            var zip = ZipFile.Open(fileName, ZipArchiveMode.Create);
+            foreach (var file in source.GetFiles())
+            {
+                // Add the entry for each filezip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
+                zip.CreateEntryFromFile(file.Name, file.FullName, CompressionLevel.Fastest);
+            }
+            // Dispose of the object when we are done
+            zip.Dispose();
+        }
+        
+        private void RunProcess(string cmd,DirectoryInfo source, DirectoryInfo target)
+        {            
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                RunProcess(cmd, dir, target.CreateSubdirectory(dir.Name));
+            }
+
+            var newTask = Task.Factory.StartNew(() =>
+            {
+
+                Parallel.ForEach(source.GetFiles(), file =>
+                {
+                    switch (cmd)
+                    {
+                        case "0":
+                            Encrypt(string.Format(@"\\?\{0}", file.FullName), string.Format(@"\\?\{0}\{1}", target.FullName, file.Name));
+                            break;
+
+                        case "1":
+                            Decrypt(string.Format(@"\\?\{0}", file.FullName), string.Format(@"\\?\{0}\{1}", target.FullName, file.Name));
+                            break;
+                    }
+                });
+            });
+
+            _tasks.Add(newTask);
+        }
+
+        private void Test()
+        {
+            string reallyLongDirectory = @"C:\Test\abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            reallyLongDirectory = reallyLongDirectory + @"\abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            reallyLongDirectory = reallyLongDirectory + @"\abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            Console.WriteLine($"Creating a directory that is {reallyLongDirectory.Length} characters long");
+            Directory.CreateDirectory(reallyLongDirectory);
+        }
+        #endregion
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _log = new Logging("EncryptionTest.log");
+
+            txtToEncrypt.Text = @"D:\TestFiles";
+            txtOutputFolder.Text = @"D:\MoveTest";
+        }
+
+        private void RunProcess_Click(object sender, EventArgs e)
+        {
+            _tasks = new List<Task>();
+            Button cmd = (Button)sender;
+
+            if (string.IsNullOrEmpty(txtToEncrypt.Text))
+            {
+                MessageBox.Show("You must enter a path to encrypt");
+                txtToEncrypt.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtOutputFolder.Text))
+            {
+                MessageBox.Show("You must enter an output path");
+                txtOutputFolder.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtEncryptionKey.Text))
+            {
+                MessageBox.Show("You must enter an encryption key.  Please keep this in a safe place as it will be required to decrypt files.");
+                txtEncryptionKey.Focus();
+                return;
+            }
+
+
+            if (!string.IsNullOrEmpty(txtToEncrypt.Text))
+            {
+                DisableControls();
+
+                DateTime startTime = DateTime.Now;
+                WriteLog(string.Format("********************** Starting Process at {0}", DateTime.Now.ToShortTimeString()));
+
+                DirectoryInfo source = new DirectoryInfo(txtToEncrypt.Text);
+                DirectoryInfo target = new DirectoryInfo(txtOutputFolder.Text);
+                foreach (DirectoryInfo dir in source.GetDirectories())
+                {
+                    RunProcess(cmd.Tag.ToString(), dir, target.CreateSubdirectory(dir.Name));
+                }
+
+                Task.WaitAll(_tasks.ToArray());
+
+                WriteLog(string.Format("********************** Ending Process at {0}", DateTime.Now.ToShortTimeString()));
+                DateTime stopTime = DateTime.Now;
+
+                MessageBox.Show("Process Complete");
+                WriteLog(string.Format("StartTime: {0} StopTime: {1}", startTime.ToShortTimeString(), stopTime.ToShortTimeString()));
+
+                EnableControls();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {            
+            this.Close();
+        }
+
+        private void btnEncryptFldDlg_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtToEncrypt.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtOutputFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
